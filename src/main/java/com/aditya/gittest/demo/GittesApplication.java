59ed62021f67868ac7a6418d485c9bc8d73387ef
Aditya <adityadudhal97@gmail.com>
2022-11-15 11:07:41 +0530

    feat: initializing files

---
+++ src/main/java/com/aditya/gittest/demo/GittesApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.gittest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/gittest/demo/GittesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.gittest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
